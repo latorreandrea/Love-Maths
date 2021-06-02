7ac4f1cfa4ad16e55016912cbfa0a8addb48026c
latorreandrea <latorre.andrea.93@gmail.com>
2021-06-02 14:14:46 +0000

    display addition for user

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,19 +5,32 @@ document.addEventListener("DOMContentLoaded",function(){
     for (let button of buttons){
         button.addEventListener("click",function() {
             if(this.getAttribute("data-type") === "submit"){
-                alert("You clicked Submit!")
+                alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+
+    runGame("addition");
 })
 
+/**
+ * CREATE A LOOP FOR START GAME
+ */
 
-function runGame(){
+ /** CREATE TWO RANDOM NUMBERS BETWEEN 1 AND 25 */
+function runGame(gameType){
     let num1 = Math.floor(Math.random()*25)+1;
     let num2 = Math.floor(Math.random()*25)+1;
+
+    if (gameType === "addition"){
+        displayAdditionQuestion(num1, num2)
+    }else{
+        alert(`Unknow game type ${gameType}`);
+        throw `Unknow game type ${gameType}. Aborting!`;
+    }
 
 }
 
@@ -37,8 +50,10 @@ function incrementWrongAnswer(){
 
 }
 
-function displayAdditionQuestion(){
-
+function displayAdditionQuestion(operand1, operand2){
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
 }
 
 function displaySubstractQuestion(){
