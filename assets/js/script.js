08ac888c5c2d02984b3af7f0c8c5e3c7c600589a
latorreandrea <latorre.andrea.93@gmail.com>
2021-06-02 12:57:55 +0000

    add basic structure for script file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,33 @@
 console.log("Benvenuto! Welcome! Willkommen! Bienvenue!")
+
+function runGame(){
+
+}
+
+function checkAnswer(){
+
+}
+
+function calculateCorrectAnswer(){
+
+}
+
+function incrementScore(){
+
+}
+
+function incrementWrongAnswer(){
+
+}
+
+function displayAdditionQuestion(){
+
+}
+
+function displaySubstractQuestion(){
+
+}
+
+function displayMultiplyQuestion(){
+
+}
